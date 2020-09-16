9ebfd8b12d1832659bcc648ea6ec196cec9cf44f
doutt <tingting.dou@hiroop.com>
2020-09-16 09:32:27 +0800

    merge

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,2 +1,2 @@
-const URL = process.env.NODE_ENV === 'production' ? 'https://yourdomain/' : 'http://localhost:21021/';
+const URL = process.env.NODE_ENV === 'production' ? 'https://yourdomain/' : 'http://47.116.131.89/';
 export default URL;
