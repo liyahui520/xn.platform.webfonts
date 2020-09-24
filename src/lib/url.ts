fe3839aa46608dc3317d87740dc3a8fc007951b7
liyahui520 <liyahui360@163.com>
2020-09-24 09:30:20 +0800

    测试环境地址

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,2 +1,2 @@
-const URL = process.env.NODE_ENV === 'production' ? 'http://47.116.131.89:8088/' : 'http://47.116.131.89:8088/';
+const URL = process.env.NODE_ENV === 'production' ? 'http://47.116.131.89:8088/' : 'http://localhost:21021/';
 export default URL;
