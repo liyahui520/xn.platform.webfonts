6a43181ebc4a8c40bf850c0abb3b14c4acddcea3
李亚辉 <liyahui360@163.com>
2021-02-08 14:08:30 +0800

    测试环境

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,2 +1,2 @@
-const URL = process.env.NODE_ENV === 'production' ? 'https://dc-api.warmsoft.com/' : 'https://dc-api.warmsoft.com/';
+const URL = process.env.NODE_ENV === 'production' ? 'https://dc-api.warmsoft.com/' : 'http://localhost:21021/';
 export default URL;
